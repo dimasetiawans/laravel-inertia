14d033b6ba256fbf98de3a64b5ef37d5a9fc6c4a
dimasetiawans <office.dimasss@gmail.com>
2025-11-13 11:28:41 +0700

    create app entry point

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,22 @@
-import './bootstrap';
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
+
+import Layout from '@/Layout/Layout.vue'
+
+createInertiaApp({
+  resolve: name => {
+    const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+    let page = pages[`./Pages/${name}.vue`]
+    page.default.layout = name.startsWith('Public/') ? undefined : Layout
+    return page
+  },
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .mount(el);
+  },
+});
+
+
+
