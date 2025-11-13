4e721f41e162036a343edba3ee289ef69008a886
dimasetiawans <office.dimasss@gmail.com>
2025-11-13 11:29:53 +0700

    define a new route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [UserController::class, 'show']);
+
