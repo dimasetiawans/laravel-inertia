b9011ee561b9741197ec818a80288164fdfe9f79
dimasetiawans <office.dimasss@gmail.com>
2025-11-19 14:47:46 +0700

    create button & form component

---
+++ resources/js/Components/Base/BaseInput.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+defineProps({
+    id: String,
+    label: String,
+    type: {
+        type: String,
+        default: "text",
+    },
+});
+
+const modelValue = defineModel();
+</script>
+
+<template>
+    <div class="form-floating mb-3">
+        <input
+            :id="id"
+            :type="type"
+            class="form-control"
+            v-model="modelValue"
+            v-bind="$attrs"
+        />
+         <label :for="id"> {{ label }}</label>
+    </div>
+</template>
