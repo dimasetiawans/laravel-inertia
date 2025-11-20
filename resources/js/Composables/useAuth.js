8e92b7a28a42bc1c3790bf1f09dca0d7ec39596e
dimasetiawans <office.dimasss@gmail.com>
2025-11-20 09:33:40 +0700

    create login logic

--- resources/js/Composables/useAuth.js
+++ resources/js/Composables/useAuth.js
@@ -1,6 +1,11 @@
 import { useForm } from '@inertiajs/vue3'
 
 export function useAuth() {
+  const loginForm = useForm({
+    email: '',
+    password: '',
+  })
+
   const registerForm = useForm({
     name: '',
     email: '',
@@ -8,7 +13,8 @@ export function useAuth() {
     password_confirmation: '',
   })
 
+  const login = () => loginForm.post('/login')
   const register = () => registerForm.post('/register')
   
-  return { registerForm, register }
+  return { loginForm, registerForm, login, register }
 }
