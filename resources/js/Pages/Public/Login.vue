9c9ead6a8c814b56cf518e50edc35c25e6ca263a
dimasetiawans <office.dimasss@gmail.com>
2025-11-20 09:35:11 +0700

    implement form, button, & alert components and auth & flash composables

--- resources/js/Pages/Public/Login.vue
+++ resources/js/Pages/Public/Login.vue
@@ -1,89 +1,52 @@
 <script setup>
-import { useForm, usePage } from "@inertiajs/vue3";
+import { useAuth } from "@/Composables/useAuth";
+const { loginForm, login } = useAuth();
 
-const form = useForm({
-    email: "",
-    password: "",
-    remember: false,
-});
+import { useFormHandler } from "@/Composables/useFormHandler";
+const { flash } = useFormHandler();
 
-const submit = () => {
-    form.post("/login");
-};
-
-const page = usePage();
+import BaseInput from "@/Components/Base/BaseInput.vue";
+import BaseButton from "@/Components/Base/BaseButton.vue";
+import BaseAlert from "@/Components/Base/BaseAlert.vue";
 </script>
 
 <template>
     <body class="text-center">
         <main class="form-signin">
-            <form @submit.prevent="submit">
-                <!-- Logo -->
-                <img
-                    class="mb-4"
-                    src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg"
-                    alt=""
-                    width="72"
-                    height="57"
+            <img
+                class="mb-4"
+                src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg"
+                alt=""
+                width="72"
+                height="57"
+            />
+
+            <BaseAlert
+                type="success"
+                :message="flash.success"
+            />
+
+            <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+            <form @submit.prevent="login">
+                <BaseInput
+                    id="email"
+                    label="Email"
+                    placeholder="Email"
+                    v-model="loginForm.email"
                 />
-
-                <!-- Show flash success -->
-                <div
-                    v-if="page.props.flash.success"
-                    class="alert alert-success mb-3"
-                >
-                    {{ page.props.flash.success }}
-                </div>
-
-                <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-
-                <!-- Email -->
-                <div class="form-floating mb-2">
-                    <input
-                        type="email"
-                        v-model="form.email"
-                        class="form-control"
-                        id="floatingInput"
-                        placeholder="name@example.com"
-                    />
-                    <label for="floatingInput">Email address</label>
-
-                    <div class="text-danger small" v-if="form.errors.email">
-                        {{ form.errors.email }}
-                    </div>
-                </div>
-
-                <!-- Password -->
-                <div class="form-floating mb-3">
-                    <input
-                        type="password"
-                        v-model="form.password"
-                        class="form-control"
-                        id="floatingPassword"
-                        placeholder="Password"
-                    />
-                    <label for="floatingPassword">Password</label>
-                    <div class="text-danger small" v-if="form.errors.password">
-                        {{ form.errors.password }}
-                    </div>
-                </div>
-
-                <!-- Remember me -->
-                <div class="checkbox mb-3">
-                    <label>
-                        <input type="checkbox" v-model="form.remember" />
-                        Remember me
-                    </label>
-                </div>
-
-                <!-- Submit button -->
-                <button
-                    class="w-100 btn btn-lg btn-primary"
-                    :disabled="form.processing"
-                    type="submit"
-                >
-                    Sign in
-                </button>
+                <BaseAlert 
+                    type="danger"
+                    :message="loginForm.errors.email"
+                />
+                <BaseInput 
+                    id="password"
+                    label="Password"
+                    placeholder="Password"
+                    v-model="loginForm.password"
+                    type="password"
+                />
+                <BaseAlert type="danger" :message="loginForm.errors.password" />
+                <BaseButton type="submit">Login</BaseButton>
             </form>
         </main>
     </body>
