28df5d6941da25c346a677d8c6fd8b4a0cfbba54
dimasetiawans <office.dimasss@gmail.com>
2025-11-20 08:49:22 +0700

    create alert component

---
+++ resources/js/Components/Base/BaseAlert.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  type: {
+    type: String,
+    default: "danger" // danger, success, warning, info
+  },
+  message: {
+    type: String,
+    default: ""
+  }
+})
+
+const alertClass = computed(() => {
+  return `alert alert-${props.type} py-2`
+})
+</script>
+
+<template>
+  <div v-if="message" :class="alertClass" role="alert">
+    {{ message }}
+  </div>
+</template>
